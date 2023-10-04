293384fd87d5b117b3008d7734d93b5f5787ae5c
Danial Ali <44068314+TheOGApplePie@users.noreply.github.com>
2023-10-04 00:00:00 -0400

    Revert "trying a fix"

--- infinite.js
+++ infinite.js
@@ -1,19 +1,10 @@
 let lastKnownY = 0;
-// document.addEventListener("scroll",(event)=>{
-// if(window.scrollY === window.innerHeight*4 && lastKnownY < window.scrollY){
-//     window.scrollTo(0,0);
-// }
-// else if(window.scrollY === 0 && lastKnownY > window.scrollY){
-// window.scrollTo(0,window.innerHeight*4);
-// }
-// lastKnownY = window.scrollY;
-// });
-document.addEventListener("touchmove",(event)=>{
-    if(window.scrollY === window.innerHeight*4 && lastKnownY < window.scrollY){
-        window.scrollTo(0,0);
-    }
-    else if(window.scrollY === 0 && lastKnownY > window.scrollY){
-    window.scrollTo(0,window.innerHeight*4);
-    }
-    lastKnownY = window.scrollY;
-    });
+document.addEventListener("scroll",(event)=>{
+if(window.scrollY === window.innerHeight*4 && lastKnownY < window.scrollY){
+    window.scrollTo(0,0);
+}
+else if(window.scrollY === 0 && lastKnownY > window.scrollY){
+window.scrollTo(0,window.innerHeight*4);
+}
+lastKnownY = window.scrollY;
+});
